4a178b0c894514bcd027717c591d32474a57cacc
Mihai Serban <mihaiserban2@gmail.com>
2019-05-22 12:05:07 +0300

    external link new tab

--- src/components/link.js
+++ src/components/link.js
@@ -4,6 +4,7 @@ import { Link as GatsbyLink } from 'gatsby';
 const Link = ({ children, to, ...props }) => {
   const internal = /^\/(?!\/)/.test(to);
 
+  console.log(to, internal);
   if (internal) {
     return (
       <GatsbyLink to={to} {...props} style={{ width: 'fit-content' }}>
@@ -13,7 +14,13 @@ const Link = ({ children, to, ...props }) => {
   }
 
   return (
-    <a href={to} {...props} style={{ width: 'fit-content' }}>
+    <a
+      href={to}
+      {...props}
+      style={{ width: 'fit-content' }}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
       {children}
     </a>
   );

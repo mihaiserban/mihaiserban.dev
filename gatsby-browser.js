21da37be33796f0332510618fe2c3c15ef3472d0
Mihai Serban <mihaiserban2@gmail.com>
2020-01-01 22:56:50 +0200

    remove service worker confirmation message

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,11 +4,5 @@ import "./src/styles/global.css";
 import "./src/styles/layout.css";
 
 export const onServiceWorkerUpdateReady = () => {
-  const answer = window.confirm(
-    `mihaiserban.dev has been updated. ` + `Reload to display the latest version?`
-  );
-
-  if (answer === true) {
-    window.location.reload();
-  }
+  window.location.reload();
 };

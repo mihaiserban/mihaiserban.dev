901a76d1629ba0aed4e5c49e3740720d8d8fcd64
Mihai Serban <mihaiserban2@gmail.com>
2020-04-15 22:04:52 +0300

    fix build

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,28 +1,21 @@
-require('dotenv').config({
-  path: `.env.${process.env.NODE_ENV}`,
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`
 });
 
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://mihaiserban.dev`,
+    siteUrl: `https://mihaiserban.dev`
   },
   plugins: [
-    'gatsby-plugin-styled-jsx',
-    'gatsby-plugin-dark-mode',
+    "gatsby-plugin-styled-jsx",
+    "gatsby-plugin-dark-mode",
     // Adding various source folders to the GraphQL layer.
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/src/pages/`,
-        name: `pages`,
-      },
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/src/images/`,
-        name: `images`,
-      },
+        name: `pages`
+      }
     },
     {
       resolve: `gatsby-plugin-gtag`,
@@ -31,20 +24,20 @@ module.exports = {
         // Puts tracking script in the head instead of the body
         head: true,
         // enable ip anonymization
-        anonymize: true,
-      },
+        anonymize: true
+      }
     },
     {
-      resolve: 'gatsby-source-contentful',
+      resolve: "gatsby-source-contentful",
       options: {
-        spaceId: process.env.CONTENTFUL_SPACE_ID || '',
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || '',
-        downloadLocal: true,
-      },
+        spaceId: process.env.CONTENTFUL_SPACE_ID || "",
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || "",
+        downloadLocal: true
+      }
     },
 
     {
-      resolve: 'gatsby-transformer-remark',
+      resolve: "gatsby-transformer-remark",
       options: {
         plugins: [
           {
@@ -57,47 +50,47 @@ module.exports = {
               // you may use this to prevent Prism from re-processing syntax.
               // This is an uncommon use-case though;
               // If you're unsure, it's best to use the default value.
-              classPrefix: 'language-',
+              classPrefix: "language-",
               // This is used to allow setting a language for inline code
               // (i.e. single backticks) by creating a separator.
               // This separator is a string and will do no white-space
               // stripping.
               // A suggested value for English speakers is the non-ascii
               // character '›'.
-              inlineCodeMarker: '›',
+              inlineCodeMarker: "›",
               // This lets you set up language aliases.  For example,
               // setting this to '{ sh: "bash" }' will let you use
               // the language "sh" which will highlight using the
               // bash highlighter.
-              aliases: {},
-            },
-          },
-        ],
-      },
+              aliases: {}
+            }
+          }
+        ]
+      }
     },
     `gatsby-plugin-sitemap`,
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
-    'gatsby-transformer-json',
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-styled-components',
+    "gatsby-transformer-json",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-styled-components",
     `gatsby-plugin-subfont`,
     {
       resolve: `gatsby-plugin-netlify`,
       options: {
         headers: {
-          '/*.js': ['cache-control: public, max-age=31536000, immutable'],
-          '/*.css': ['cache-control: public, max-age=31536000, immutable'],
-          '/sw.js': ['cache-control: public, max-age=0, must-revalidate'],
-        },
-      },
+          "/*.js": ["cache-control: public, max-age=31536000, immutable"],
+          "/*.css": ["cache-control: public, max-age=31536000, immutable"],
+          "/sw.js": ["cache-control: public, max-age=0, must-revalidate"]
+        }
+      }
     },
     {
       resolve: `gatsby-plugin-typography`,
       options: {
-        pathToConfigModule: `src/utils/typography`,
-      },
-    },
-  ],
+        pathToConfigModule: `src/utils/typography`
+      }
+    }
+  ]
 };
